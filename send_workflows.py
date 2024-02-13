02805b492cf456f667a931fc233f1d20f582c9fa
Chukwuemeka Muonagor <c.muonagor@tu-braunschweig.de>
2024-02-13 06:40:05 +0000

    Delete send_workflows.py

--- send_workflows.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-#the function that sends the workflow to the workflow api
-def send_workflow_fun(workflow_url, workflow):
-    requests.put(workflow_url, json=workflow)
-    #print(response.json())
-    print('sent workflow: ', workflow)
