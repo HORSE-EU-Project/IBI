fb62270d124aee8529e8e62d881ed2741c56ddc5
Chukwuemeka Muonagor <c.muonagor@tu-braunschweig.de>
2024-02-13 07:19:10 +0000

    Upload New File

---
+++ app/run_whatif_loop.py
@@ -0,0 +1,17 @@
+from elasticsearch import Elasticsearch
+import whatif_loop
+import yaml
+
+with open('/code/app/config.yml') as f:
+    parameters = yaml.safe_load(f)
+print('parameters: ', parameters)
+host = parameters['ip']
+port = parameters['port']
+elastic_host = parameters['elasticsearch_ip']
+elastic_port = parameters['elasticsearch_port']
+elasticsearch_url = "http://" + elastic_host + ":" + elastic_port
+es = Elasticsearch(elasticsearch_url)
+#whatif_send_url = "http://" + host + ":" + port + parameters['to_send_whatif']
+whatif_send_url = parameters['san_api_url']
+
+whatif_loop.whatif_loop_fun(es, whatif_send_url)
