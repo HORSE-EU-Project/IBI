2eda6eb6fdb348060b9d32262afd1c317bcb958c
Chukwuemeka Muonagor <c.muonagor@tu-braunschweig.de>
2024-02-14 14:10:01 +0000

    Update run_whatif_loop.py

--- app/run_whatif_loop.py
+++ app/run_whatif_loop.py
@@ -1,6 +1,8 @@
 from elasticsearch import Elasticsearch
 import whatif_loop
 import yaml
+import warnings
+warnings.filterwarnings('ignore')
 
 with open('/code/app/config.yml') as f:
     parameters = yaml.safe_load(f)
