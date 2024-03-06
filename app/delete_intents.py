154d8eeae3dce7471b162c8870f5524f8e6b0bd6
Chukwuemeka Muonagor <c.muonagor@tu-braunschweig.de>
2024-03-06 15:27:51 +0000

    Update delete_intents.py

--- app/delete_intents.py
+++ app/delete_intents.py
@@ -1,29 +1,20 @@
 import delete_command
 from elasticsearch import Elasticsearch
 import delete_intents_elasticsearch
-import yaml
 import time
+import config
 
 #function for deleting intents
 def select_delete_fun(to_delete):
-    with open('/code/app/config.yml') as f:
-        parameters = yaml.safe_load(f)
-    elastic_host = parameters['elasticsearch_ip']
-    elastic_port = parameters['elasticsearch_port']
-
-    #workflow_url = "http://" + ip + ":" + port + parameters['to_send_workflow']
-    workflow_url = parameters['rtr_api_url']
-    elasticsearch_url = "http://" + elastic_host + ":" + elastic_port
+    workflow_url = config.workflow_url
+    elasticsearch_url = config.elasticsearch_url
     es = Elasticsearch(elasticsearch_url)
 
     print('to delete intent')
 
     resp = es.search(index="stored_intents", size=100, query={"match_all": {}})
-    #print('len of hits hits: ', len(resp['hits']['hits']))
-    #print(' ')
     time.sleep(1)
     for ind in range(len(resp['hits']['hits'])):
-        #print('ind: ', ind)
         intent_index = es.exists(index="stored_intents", id=1)
         # repeat the process for the next intent in the intent store
         if ind < len(resp['hits']['hits']) and intent_index == True:
@@ -36,4 +27,3 @@ def select_delete_fun(to_delete):
                 # delete intent on elasticsearch
                 delete_intents_elasticsearch.delete_intents_elasticsearch_fun(elasticsearch_url,
                                                                         resp['hits']['hits'][ind]['_id'], "stored_intents")
-
