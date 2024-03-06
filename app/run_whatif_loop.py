dc72f2ec1fa5e8b52c65609a5ed1056cbd34f444
Chukwuemeka Muonagor <c.muonagor@tu-braunschweig.de>
2024-03-06 15:39:38 +0000

    Update run_whatif_loop.py

--- app/run_whatif_loop.py
+++ app/run_whatif_loop.py
@@ -1,19 +1,11 @@
 from elasticsearch import Elasticsearch
 import whatif_loop
-import yaml
 import warnings
+import config
 warnings.filterwarnings('ignore')
 
-with open('/code/app/config.yml') as f:
-    parameters = yaml.safe_load(f)
-print('parameters: ', parameters)
-host = parameters['ip']
-port = parameters['port']
-elastic_host = parameters['elasticsearch_ip']
-elastic_port = parameters['elasticsearch_port']
-elasticsearch_url = "http://" + elastic_host + ":" + elastic_port
-es = Elasticsearch(elasticsearch_url)
-#whatif_send_url = "http://" + host + ":" + port + parameters['to_send_whatif']
-whatif_send_url = parameters['san_api_url']
-
-whatif_loop.whatif_loop_fun(es, whatif_send_url)
+def run_whatif_loop_fun():
+    elasticsearch_url = config.elasticsearch_url
+    es = Elasticsearch(elasticsearch_url)
+    whatif_send_url = config.whatif_send_url
+    whatif_loop.whatif_loop_fun(es, whatif_send_url)
