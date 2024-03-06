9e0983f52e3b0f8312eb84d1a88ba2a2b15b3024
Chukwuemeka Muonagor <c.muonagor@tu-braunschweig.de>
2024-03-06 15:23:07 +0000

    Update conflict_resolution.py

--- app/conflict_resolution.py
+++ app/conflict_resolution.py
@@ -1,14 +1,12 @@
 import requests
 import time
-from elasticsearch import Elasticsearch
-import get_intents
+import get_intents_script
 
 #function to resolve conflict in policies
 def conflict_fun(ind, policy_dict, workflow_url, stored_intents_url, elasticsearch_url):
-    es = Elasticsearch(elasticsearch_url)
     # intent_host_arr is the array containing the host(s) for which the intent(s) apply
     intent_host_arr = policy_dict['host']
-    stored_intents_arr = get_intents.get_intent_fun(stored_intents_url)
+    stored_intents_arr = get_intents_script.get_intent_fun(stored_intents_url)
     conflict_res = False
     if stored_intents_arr[ind]['host'] in intent_host_arr and \
             stored_intents_arr[ind]['threat'] == policy_dict['threat'] and \
@@ -17,7 +15,6 @@ def conflict_fun(ind, policy_dict, workflow_url, stored_intents_url, elasticsear
             if stored_intents_arr[ind]['host'] == intent_host_arr[i] and \
                     stored_intents_arr[ind]['threat'] == policy_dict['threat'] and \
                     int(policy_dict['priority']) < int(stored_intents_arr[ind]['priority']):
-                #st_dict = stored_intents_arr[ind]
                 url_to_delete = stored_intents_url + "/" + str(stored_intents_arr[ind]['id'])
                 requests.delete(url_to_delete)
                 conflict_res = True
@@ -26,11 +23,8 @@ def conflict_fun(ind, policy_dict, workflow_url, stored_intents_url, elasticsear
             time.sleep(1)
 
         if conflict_res == True:
-            stored_intents_arr = get_intents.get_intent_fun(stored_intents_url)
+            stored_intents_arr = get_intents_script.get_intent_fun(stored_intents_url)
             #repeat the process for the next intent in the intent store
-            #print('ind inside conflict res = true: ', ind)
-            #print('len(resp1[hits hits): ', len(stored_intents_arr))
-            #print('')
             if (ind < len(stored_intents_arr)):
                 print('starting new conflict resolution')
                 conflict_fun(ind, policy_dict, workflow_url, stored_intents_url,
@@ -39,10 +33,7 @@ def conflict_fun(ind, policy_dict, workflow_url, stored_intents_url, elasticsear
                 print('conflicts resolved')
         else:
             ind = ind + 1
-            stored_intents_arr = get_intents.get_intent_fun(stored_intents_url)
-            #print('ind inside else of conflict res = true: ', ind)
-            #print('len(resp1[hits hits): ', len(stored_intents_arr))
-            #print('')
+            stored_intents_arr = get_intents_script.get_intent_fun(stored_intents_url)
             # repeat the process for the next intent in the intent store
             if (ind < len(stored_intents_arr)):
                 print('starting new conflict resolution')
@@ -54,10 +45,7 @@ def conflict_fun(ind, policy_dict, workflow_url, stored_intents_url, elasticsear
     #if an existing intent doesn't conflict with new intent then move to the next intent in the intent store
     else:
         ind = ind + 1
-        stored_intents_arr = get_intents.get_intent_fun(stored_intents_url)
-        #print('ind inside else of every: ', ind)
-        #print('len(resp1[hits hits): ', len(stored_intents_arr))
-        #print('')
+        stored_intents_arr = get_intents_script.get_intent_fun(stored_intents_url)
         # repeat the process for the next intent in the intent store
         if (ind < len(stored_intents_arr)):
             print('starting new conflict resolution')
@@ -65,5 +53,4 @@ def conflict_fun(ind, policy_dict, workflow_url, stored_intents_url, elasticsear
                          elasticsearch_url)
         else:
             print('ok')
-
 
