69d8accd79e70c5d46b4aca271650178190cb7a7
izacarias <i.zacarias@tu-braunschweig.de>
2025-06-24 16:26:10 +0200

    Moving config files to root application directory.

--- app/config.py
+++ app/config.py
@@ -1,40 +1,49 @@
 import yaml
+import os
 from elasticsearch import Elasticsearch
 
-files_directory = '/code/app/'
-yml_file = files_directory + 'config.yml'
+"""
+Get current directory to derive application directory
+Config file, template folder and Q-Learning files are 
+placed a the project root directory
+"""
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Remove the /app from the tail of the path
+files_directory = os.path.dirname(current_dir)
+
+yml_file = os.path.join(files_directory, "config.yml")
 with open(yml_file) as f:
     parameters = yaml.safe_load(f)
-host = parameters['ip']
-port = parameters['port']
-elastic_host = parameters['elasticsearch_ip']
-elastic_port = parameters['elasticsearch_port']
+host = parameters["ip"]
+port = parameters["port"]
+elastic_host = parameters["elasticsearch_ip"]
+elastic_port = parameters["elasticsearch_port"]
 elasticsearch_url = "http://" + elastic_host + ":" + elastic_port
 es = Elasticsearch(elasticsearch_url)
 
-whatif_receive_url = "http://" + host + ":" + port + parameters['to_receive_whatif']
-whatif_send_url = parameters['san_api_url']
-workflow_url = parameters['rtr_api_url']
-intents_url = parameters['intents_url']
-alerts_url = parameters['alerts_url']
-stored_intents_url = parameters['stored_intents_url']
-qos_intents_url = parameters['qos_intents_url']
-stored_qos_intents_url = parameters['stored_qos_intents_url']
-rtr_username = parameters['rtr_username']
-rtr_password = parameters['rtr_password']
-rtr_email = parameters['rtr_email']
-to_connect_to_rtr = parameters['to_connect_to_rtr']
+whatif_receive_url = "http://" + host + ":" + port + parameters["to_receive_whatif"]
+whatif_send_url = parameters["san_api_url"]
+workflow_url = parameters["rtr_api_url"]
+intents_url = parameters["intents_url"]
+alerts_url = parameters["alerts_url"]
+stored_intents_url = parameters["stored_intents_url"]
+qos_intents_url = parameters["qos_intents_url"]
+stored_qos_intents_url = parameters["stored_qos_intents_url"]
+rtr_username = parameters["rtr_username"]
+rtr_password = parameters["rtr_password"]
+rtr_email = parameters["rtr_email"]
+to_connect_to_rtr = parameters["to_connect_to_rtr"]
 
-templates_directory = files_directory + parameters['templates_directory']
-static_directory = files_directory + parameters['static_directory']
-policy_store_directory = files_directory + parameters['policy_store_file']
+templates_directory = files_directory + parameters["templates_directory"]
+static_directory = files_directory + parameters["static_directory"]
+policy_store_directory = files_directory + parameters["policy_store_file"]
 
-ddos_ntp = parameters['ddos_ntp']
-ddos_dns = parameters['ddos_dns']
-ddos_pfcp = parameters['ddos_pfcp']
+ddos_ntp = parameters["ddos_ntp"]
+ddos_dns = parameters["ddos_dns"]
+ddos_pfcp = parameters["ddos_pfcp"]
 
-rate_req = parameters['req/s']
-qos_requirements = parameters['qos_requirements']
+rate_req = parameters["req/s"]
+qos_requirements = parameters["qos_requirements"]
 
-hosts = parameters['hosts']
-links = parameters['links']
+hosts = parameters["hosts"]
+links = parameters["links"]
