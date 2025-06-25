9b0f437cec4ccb36574b9008cdce39d14bd6d573
izacarias <i.zacarias@tu-braunshweig.de>
2025-06-26 00:24:20 +0200

    Creating a class to retrieve a ES connection

--- app/db/elastic_search.py
+++ app/db/elastic_search.py
@@ -1,8 +1,21 @@
 from elasticsearch import Elasticsearch
 import config
 
-es_client = Elasticsearch(
-    config.elasticsearch_url, 
-    retry_on_timeout=True,
-    max_retries=3
-)
+class ElasticSearchClient:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(ElasticSearchClient, cls).__new__(cls)
+            cls._instance._init_client()
+        return cls._instance
+
+    def _init_client(self):
+        self.client = Elasticsearch(
+            config.elasticsearch_url,
+            retry_on_timeout=True,
+            max_retries=3
+        )
+
+    def get_client(self):
+        return self.client
