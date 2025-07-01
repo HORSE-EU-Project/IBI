5bc0cee4c5ba9a6c8531942d2a006a0949c1513a
izacarias <i.zacarias@tu-braunschweig.de>
2025-07-01 18:39:25 +0200

    Addign config parameters for cKB

--- app/config.py
+++ app/config.py
@@ -23,37 +23,45 @@ with open(yml_file) as f:
 # Elasticsearch parameters
 ES_HOST = parameters["elasticsearch"]["ip"]
 ES_PORT = parameters["elasticsearch"]["port"]
-ES_CLEAN = parameters["elasticsearch"]["reset"]
 ES_URL = f"http://{ES_HOST}:{ES_PORT}"
+ES_CLEAN = parameters["elasticsearch"]["reset"]
 
 
 # Integration with the cKB
 CKB_URL = parameters["ckb"]["url"]
 
+# Integration with the Digital Twin
+IADT_URL = parameters["iadt"]["url"]
+
+# Integration with the RTR
+RTR_URL = parameters["rtr"]["url"]
+RTR_USER = parameters["rtr"]["username"]
+RTR_PASSWORD = parameters["rtr"]["password"]
+RTR_EMAIL = parameters["rtr"]["email"]
 
 # whatif_receive_url = "http://" + host + ":" + port + parameters["to_receive_whatif"]
-whatif_send_url = parameters["san_api_url"]
-workflow_url = parameters["rtr_api_url"]
-intents_url = parameters["intents_url"]
-alerts_url = parameters["alerts_url"]
-stored_intents_url = parameters["stored_intents_url"]
-qos_intents_url = parameters["qos_intents_url"]
-stored_qos_intents_url = parameters["stored_qos_intents_url"]
-rtr_username = parameters["rtr_username"]
-rtr_password = parameters["rtr_password"]
-rtr_email = parameters["rtr_email"]
-to_connect_to_rtr = parameters["to_connect_to_rtr"]
+# whatif_send_url = parameters["san_api_url"]
+# workflow_url = parameters["rtr_api_url"]
+# intents_url = parameters["intents_url"]
+# alerts_url = parameters["alerts_url"]
+# stored_intents_url = parameters["stored_intents_url"]
+# qos_intents_url = parameters["qos_intents_url"]
+# stored_qos_intents_url = parameters["stored_qos_intents_url"]
+# rtr_username = parameters["rtr_username"]
+# rtr_password = parameters["rtr_password"]
+# rtr_email = parameters["rtr_email"]
+# to_connect_to_rtr = parameters["to_connect_to_rtr"]
 
-templates_directory = files_directory + parameters["templates_directory"]
-static_directory = files_directory + parameters["static_directory"]
-policy_store_directory = files_directory + parameters["policy_store_file"]
+# templates_directory = files_directory + parameters["templates_directory"]
+# static_directory = files_directory + parameters["static_directory"]
+# policy_store_directory = files_directory + parameters["policy_store_file"]
 
-ddos_ntp = parameters["ddos_ntp"]
-ddos_dns = parameters["ddos_dns"]
-ddos_pfcp = parameters["ddos_pfcp"]
+# ddos_ntp = parameters["ddos_ntp"]
+# ddos_dns = parameters["ddos_dns"]
+# ddos_pfcp = parameters["ddos_pfcp"]
 
-rate_req = parameters["req/s"]
-qos_requirements = parameters["qos_requirements"]
+# rate_req = parameters["req/s"]
+# qos_requirements = parameters["qos_requirements"]
 
-hosts = parameters["hosts"]
-links = parameters["links"]
+# hosts = parameters["hosts"]
+# links = parameters["links"]
