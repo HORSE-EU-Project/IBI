bfb1c8a83a389749ca7d9c51b747d88f16f275fd
izacarias <i.zacarias@tu-braunschweig.de>
2025-07-01 16:08:56 +0200

    Setting log to DEBUG

--- app/utils/log_config.py
+++ app/utils/log_config.py
@@ -1,18 +1,21 @@
 import logging
 from constants import Const
 
-def setup_logging():
-    logger = logging.getLogger(Const.APP_NAME)
+def setup_logging(current_file=None):
+    if current_file is None:
+        current_file = Const.APP_NAME
+        
+    logger = logging.getLogger(current_file)
     logger.setLevel(logging.DEBUG)
 
     if not logger.handlers:
         # Define log format
         formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+            '%(name)s - %(levelname)s - %(asctime)s - %(message)s'
         )
         # Log to console
         console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO) # Console output typically INFO or higher
+        console_handler.setLevel(logging.DEBUG) # Console output typically INFO or higher
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
     
