e4197dbfca04af889df244e205144e54b75436ba
izacarias <i.zacarias@tu-braunschweig.de>
2025-07-01 18:47:47 +0200

    Adding recommender to the pipeline

--- app/pipeline.py
+++ app/pipeline.py
@@ -14,19 +14,27 @@ class IntentPipeline:
 
     def process_intents(self):
         # Get intents with status 'new'
+        logger.info("Starting intent pipeline iteration")
         try:
-            logger.info("Starting intent pipeline iteration")
+            logger.info("Checking new intents")
             intents = self.intent_manager.get_all(status=Const.INTENT_STATUS_NEW)
             for intent in intents:
                 logger.info(f"Processing intent {intent.get("id")} ")
+                # Set status of intent to "processing"
 
                 # Query cKB
                 ckb = CKB()
                 ckb.query_ckb(intent.get("threat"))
 
+                # Get mitigation actions from recommender
 
+                # Validate with CAS
 
+                # Send to RTR
+
+                # Set status of intent to "under mitigation"
             
         except Exception as e:
             logger.error(f"Error querying Elasticsearch: {e}")
         
+        # Set intents "under mitigation" that reached timeout to "mitigated"
---
+++ app/recommender.py
@@ -0,0 +1,9 @@
+from constants import Const
+from utils.log_config import setup_logging
+
+class Recommender:
+
+    def __init__(self):
+        pass
+
+    
