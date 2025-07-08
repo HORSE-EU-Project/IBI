885fb955b68888a5e5d0ea110c4cb848dd321791
izacarias <i.zacarias@tu-braunschweig.de>
2025-07-08 16:18:25 +0200

    Refactoring the intent entry point. PUT and POST will send to the same function.

--- app/routers/intents.py
+++ app/routers/intents.py
@@ -1,5 +1,5 @@
 import logging
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from models import SecurityIntent
 from intent_manager import IntentManager
 
@@ -13,13 +13,23 @@ async def get_intents():
 
 
 @router.post("/intents", status_code=201)
-def post_intent(intent: SecurityIntent):
+def post_intent(intent: SecurityIntent, response:Response):
     logger.debug(f"Received intent: {intent}")
-    intent_manager.add(intent)
-    return intent
+    status = intent_manager.process_intent_request(intent)
+    if status == IntentManager.INTENT_CREATED:
+        logger.info(f"Intent {intent} created successfully")
+        response.status_code = 201 
+    elif status == IntentManager.INTENT_ALREADY_EXISTS:
+        logger.warning(f"Intent {intent} already exists")
+        response.status_code = 208
+        return intent
+    else:
+        logger.error(f"Failed to create intent {intent}")
+        response.status_code = 500
+        return {"error": "Failed to create intent"}
 
 
 @router.put("/intents")
 def put_intent(intent: SecurityIntent):
-    logger.debug(f"Updating intent: {intent}")
-    return intent
+    logger.debug(f"Redirecting to post_intent for intent: {intent}")
+    post_intent(intent)
