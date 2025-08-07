f7d11889d709a887cfe5c0088fc02d74ebe6ada2
izacarias <i.zacarias@tu-braunschweig.de>
2025-08-07 11:36:31 +0200

    fix return codes in DTE routers

--- app/routers/intents.py
+++ app/routers/intents.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 router = APIRouter()
 intent_manager = DTEController()
 
+
 @router.post("/intents", status_code=201)
 def post_intent(dte_intent: DTEIntent, response:Response):
     logger.debug(f"Received intent: {dte_intent}")
@@ -14,10 +15,11 @@ def post_intent(dte_intent: DTEIntent, response:Response):
     if status == DTEController.RETURN_STATUS_CREATED:
         logger.info(f"Intent {dte_intent} created successfully")
         response.status_code = 201
+        return {"created": dte_intent}
     elif status == DTEController.RETURN_STATUS_UPDATED:
         logger.warning(f"Intent {dte_intent} already exists. Updating threat state")
         response.status_code = 208
-        return dte_intent
+        return {"info": "Intent updated"}
     else:
         logger.error(f"Failed to create intent {dte_intent}")
         response.status_code = 500
