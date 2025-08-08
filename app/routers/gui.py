2f985a5fbe0cfa76564a94dcaad5ce149d54fe7c
izacarias <i.zacarias@tu-braunschweig.de>
2025-08-08 17:31:16 +0200

    Adding GUI

---
+++ app/routers/gui.py
@@ -0,0 +1,15 @@
+from fastapi import  APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from utils.log_config import setup_logging
+
+logger = setup_logging(__file__)
+
+router = APIRouter()
+
+templates = Jinja2Templates(directory="app/gui/templates")
+
+@router.get("/")
+def get_index(request: Request):
+    return templates.TemplateResponse(name="index.html", context={"request": request})
