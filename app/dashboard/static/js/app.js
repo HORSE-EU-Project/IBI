922ada21e587e5fd60aff4b955c68770e682b41f
izacarias <i.zacarias@tu-braunschweig.de>
2025-08-11 19:45:11 +0200

    Adding dashboard

--- app/dashboard/static/js/app.js
+++ app/dashboard/static/js/app.js
@@ -1,1 +1,8 @@
 $(document).foundation()
+
+// Dashboard JavaScript
+$('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
+  e.preventDefault();
+  $(this).parents('.app-dashboard').toggleClass('shrink-medium').toggleClass('shrink-large');
+});
+
