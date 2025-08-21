3bc654ae57c13a72c680f1f07e3e1808e8293e45
izacarias <i.zacarias@tu-braunschweig.de>
2025-08-21 18:45:34 +0200

    Update threat timeout constant and add new constants for Digital Twin and CAS parameter tuning

--- app/constants.py
+++ app/constants.py
@@ -13,12 +13,17 @@ class Const:
 
     # Intent processing loop
     THREAD_INTENT_WAIT = 5.0
-    THREAT_TIMEOUT = 30 # 2.0 * 60.0  # 2 minutes
+    THREAT_TIMEOUT = 2.0 * 60.0  # 2 minutes
 
     # Elasticsearch indexes
     INTENTS_INDEX = "intents"
     MITIGATION_INDEX = "mitigations"
     ASSOCIATIONS_INDEX = "associations"
 
-    IADT_PPS_THRESHOLD = 2.0
+    # Digital Twin related constants
+    IADT_PPS_THRESHOLD = 0.5
+
+    # CAS related constants (parameter tuning)
+    CAS_RATE_MITITING_INCREMENT = 1
+
 
