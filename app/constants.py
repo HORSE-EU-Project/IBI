6f53436d86bca8544d652ede1001c5c13fb0b264
izacarias <i.zacarias@tu-braunschweig.de>
2025-09-02 15:33:34 +0200

    Fix: Wrong variable name

--- app/constants.py
+++ app/constants.py
@@ -15,16 +15,11 @@ class Const:
 
     # Server related
     APP_HOST = "0.0.0.0"
-    APP_PORT = 8000
+    APP_PORT = 8070
 
     # Intent processing loop
     THREAD_INTENT_WAIT = 5.0
     THREAT_TIMEOUT = 2.0 * 60.0  # 2 minutes
-
-    # Elasticsearch indexes
-    INTENTS_INDEX = "intents"
-    MITIGATION_INDEX = "mitigations"
-    ASSOCIATIONS_INDEX = "associations"
 
     # Digital Twin related constants
     IADT_PPS_THRESHOLD = 0.5
